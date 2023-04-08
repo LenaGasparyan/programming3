492f54e9688e6df5da9f1fd8adcf736ccc135e43
LenaGasparyan <lena.gasparyan@tumo.org>
2023-04-08 14:28:17 +0400

    JavaScript Class Inheritance

--- gameOfLife2/gishatichaspan.js
+++ gameOfLife2/gishatichaspan.js
@@ -1,7 +1,6 @@
-class Gishatichaspan {
+class Gishatichaspan extends LivingCreature{
     constructor(x, y) {
-        this.x = x
-        this.y = y
+        super(x,y)
         this.energy = 20
         this.directions = []
     }
@@ -19,19 +18,7 @@ class Gishatichaspan {
     }
     chooseCell(char4) {
         this.getNewCoordinates()
-        let found = []
-
-
-        for (let i in this.directions) {
-            let x = this.directions[i][0]
-            let y = this.directions[i][1]
-            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
-                if (matrix[y][x] == char4) {
-                    found.push(this.directions[i])
-                }
-            }
-            
-        }
+     return super.chooseCell(char4)
 
 
         return found
